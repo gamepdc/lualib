e5e310c4bc3cfbae82d3f267ff1fdacf5a1095a6
gamepdc <gamepdc@gmail.com>
2016-11-17 23:55:39 +0800

    tostring parsecmd

---
+++ util.lua
@@ -0,0 +1,63 @@
+local function plain2string(v)
+	if type(v) == "number" then
+		return tostring(v)
+	elseif type(v) == "string" then
+		return "\"" .. v .. "\""
+	else
+		error(string.format("can't supper type:%s", type(v)))
+	end
+end
+
+local function key2string(v)
+	if type(v) == "number" then
+		return "[" .. v .. "]"
+	elseif type(v) == "string" then
+		return "\"" .. v .. "\""
+	else
+		error(string.format("can't supper key type:%s", type(v)))
+	end
+end
+
+local function tb2string(tb, visited)
+	if visited == nil then
+		visited = {}
+	end
+
+	local indentStr = ""
+	for k, v in ipairs(visited) do
+		if v == tb then
+			error("loop ref")
+		end
+		indentStr = indentStr .. "    "
+	end
+	table.insert(visited, tb)
+
+	local s = "{\n"
+	local contentIndent = indentStr .. "    "
+	for k, v in pairs(tb) do
+		s = s .. contentIndent .. key2string(k) .. ": "
+		local valueStr
+		if type(v) == "table" then
+			valueStr = tb2string(v, visited)
+		else
+			valueStr = plain2string(v)
+		end
+		s = s .. valueStr .. "\n"
+	end
+	s = s .. indentStr .. "}"
+	return s
+end
+
+local function ToString(v)
+	if type(v) == "table" then
+		return tb2string(v)
+	else
+		return plain2string(v)
+	end
+end
+
+local function ParseCmd(line)
+	local cmdStr = string.gsub(line, "(%w+)%s(.+)", "return {cmd=\"%1\", param=%2}")
+	local cmd = load(cmdStr)()
+	return cmd
+end
